bc5e3818ad6fec6508ef99dffa5e443b838e7ac3
SergeyGuliy <sergey.guliy.92@gmail.com>
2019-10-26 01:32:27 +0300

    -Created changing for name in Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,14 +7,29 @@
           <h3>Профиль</h3>
         </div>
 
-        <form class="form">
+        <form class="form" v-on:submit.prevent="submitHandler">
           <div class="input-field">
-            <input id="description" type="text">
-            <label for="description">Имя</label>
-            <span class="helper-text invalid">name</span>
-          </div>
+            <input id="description" type="text" v-model="name" v-bind:class="{invalid: (!this.$v.name.required || !this.$v.name.minLength || !this.$v.name.maxLength || !this.$v.name.alpha)}">
+			  <label for="description">Имя</label>
+			  <small
+					  class="helper-text invalid"
+					  v-if="!$v.name.required"
+			  >Введите ваше имя</small>
+			  <small
+					  class="helper-text invalid"
+					  v-else-if="!$v.name.alpha"
+			  >Имя должно содержать только латинские быквы</small>
+			  <small
+					  class="helper-text invalid"
+					  v-else-if="!$v.name.minLength"
+			  >Слишком короткое имя</small>
+			  <small
+					  class="helper-text invalid"
+					  v-else-if="!$v.name.maxLength"
+			  >Слишком длинное имя</small>
+		  </div>
 
-          <button class="btn waves-effect waves-light" type="submit">
+          <button class="btn" type="submit" v-bind:class="{disabled: (!this.$v.name.required || !this.$v.name.minLength || !this.$v.name.maxLength || !this.$v.name.alpha)}">
             Обновить
             <i class="material-icons right">send</i>
           </button>
@@ -26,8 +41,33 @@
 </template>
 
 <script>
+import { required, minLength, maxLength, alpha } from 'vuelidate/lib/validators'
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  data () {
+    return {
+      	name: ''
+	  }
+  },
+  validations: {
+    name: { minLength: minLength(2), maxLength: maxLength(10), required, alpha }
+  },
+  computed: {
+    nameOld () {
+      return this.$store.getters.info.name
+	  }
+  },
+  mounted () {
+    this.name = this.nameOld
+    setTimeout(function () {
+      M.updateTextFields()
+    }, 0)
+  },
+  methods: {
+    async submitHandler () {
+      await this.$store.dispatch('updateInfo', { name: this.name })
+    }
+  }
 }
 </script>
 
